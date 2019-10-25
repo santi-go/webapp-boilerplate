b7c5cfd8dc428f4519d7087e09274f5747b9d755
Santiago Gonzalez Erazo <santiago.gonzalez.erazo@gmail.com>
2019-10-25 22:17:14 +0200

    react installed and working

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>React is working!</div>
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
