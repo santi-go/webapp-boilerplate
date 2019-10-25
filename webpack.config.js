4d54e65d6f910873c70fe6bf5e69f450981da465
Santiago Gonzalez Erazo <santiago.gonzalez.erazo@gmail.com>
2019-10-25 23:06:15 +0200

    manage static files feature

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,12 @@ module.exports = {
             {
                 test: /\.s?css$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: "file-loader",
+                options: { name: '/static/[name].[ext]' }
+              }
         ]
     }
 };
